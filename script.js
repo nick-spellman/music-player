5e541936bc3199853ff5c5181e4575dec71b1b2a
nick-spellman <nickspellman97@protonmail.com>
2024-02-18 16:50:32 -0300

    aula 06

--- script.js
+++ script.js
@@ -1,13 +1,81 @@
 const songName = document.getElementById("song-name");
 const bandName = document.getElementById("band-name");
+const cover = document.getElementById("cover");
 const song = document.getElementById("audio");
 const play = document.getElementById("play");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
 
-songName.innerText = "Titanium";
-bandName.innerText = "Alisia Beats";
+const titanium = {
+    songName: 'Titanium',
+    artist: 'Alisia Beats',
+    file: 'titanium'
+};
+const betterDay = {
+    songName: 'Better Day',
+    artist: 'Penguin Music',
+    file: 'better_day'
+};
+const midnightForest = {
+    songName: 'Midnight Forest',
+    artist: 'Syouki Takahashi',
+    file: 'midnight_forest'
+};
+let isPlaying = false;
+const playlist = [titanium, betterDay, midnightForest];
+let index = 0;
 
 function playSong() {
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     song.play();
+    isPlaying = true;
 }
 
-play.addEventListener('click', playSong);
+function pauseSong() {
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider() {
+    if (isPlaying === true) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+}
+
+function initializeSong() {
+    cover.src = `images/${playlist[index].file}.jpg`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
+}
+
+function previousSong() {
+    if (index === 0) {
+        index = playlist.length - 1;
+    } else {
+        index -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function nextSong() {
+    if (index === playlist.length - 1) {
+        index = 0;
+    } else {
+        index += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+initializeSong();
+
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
